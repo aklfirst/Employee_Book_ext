022ae55706e911a7e12c56972b51209455a4530a
Alexander Klepov <a_klepov@mail.ru>
2022-10-06 15:38:09 +0300

    Выполненное ДЗ

--- src/main/java/com/example/employee_book_ext/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employee_book_ext/service/EmployeeServiceImpl.java
@@ -6,56 +6,79 @@ import com.example.employee_book_ext.exceptions.EmployeeNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 
 public class EmployeeServiceImpl implements EmployeeService {
 
-    List<Employee> employees = new ArrayList<>(Arrays.asList(
-            new Employee("Александр", "Клепов"),
-            new Employee("Дмитрий", "Фролов"),
-            new Employee("Дмитрий", "Волосевич"),
-            new Employee("Анатолий", "Карпов"),
-            new Employee("Иван1", "Иванов"),
-            new Employee("Иван2", "ВторойИванов"),
-            new Employee("Иван3", "ТретийИванов")
+
+    public Map<String, Employee> employees = new HashMap<>(Map.of(
+           "A_Клепов", new Employee("Александр", "Клепов", 5, 200_000),
+            "Д_Фролов", new Employee("Дмитрий", "Фролов",  2, 250_000),
+            "Д_Волосевич", new Employee("Дмитрий", "Волосевич", 5, 50_000),
+            "А_Карпов", new Employee("Анатолий", "Карпов",  3, 60_000),
+            "Г_Каспаров", new Employee("Гарии", "Каспаров", 1, 90_000),
+            "А_Елькин", new Employee("Андрей", "Елькин", 1, 190_000),
+            "В_Палкин", new Employee("Василий", "Палкин", 2, 110_000),
+            "И_Иванов", new Employee("Иван1", "Иванов",  2, 120_000),
+            "И_ВторойИванов", new Employee("Иван2", "ВторойИванов", 2, 125_000),
+            "И_ТретийИванов", new Employee("Иван3", "ТретийИванов",  3, 130_000)
     ));
 
     @Override
-    public Collection<Employee> printAllEmployees() {
-      return Collections.unmodifiableList(employees);
+    public List<Employee> printAllEmployees() {
+        return employees.values().stream()
+                .sorted(Comparator.comparingInt(Employee::getDepartment))
+                .collect(Collectors.toList());
     }
 
+
     @Override
-    public Employee remove(String firstName, String lastName) throws EmployeeNotFoundException {
-        Employee forRemove = new Employee(firstName, lastName);
-        if (!employees.contains(forRemove)) {
-            throw new EmployeeNotFoundException("Такой сотрудник не найден!");
+    public List <Employee> printByDepartment(int department) {
+        return employees.values().stream()
+                .filter(e->e.getDepartment() == (department))
+                .collect(Collectors.toList());
         }
-        employees.remove(forRemove);
+
+
+    @Override
+    public Optional<Employee> remove(String key) throws EmployeeNotFoundException {
+        Optional<Employee> forRemove = find(key);
+        employees.remove(key);
         return forRemove;
     }
 
 
     @Override
-    public Employee add(String firstName, String lastName) throws EmployeeAlreadyAddedException {
-        Employee toAdd = new Employee(firstName, lastName);
-        if (employees.contains(toAdd)) {
+    public Employee add(String key, String firstName, String lastName, int department, float salary) throws EmployeeAlreadyAddedException {
+        Employee employeeToAdd = new Employee(firstName,lastName,department,salary);
+        if (employees.containsKey(key)) {
             throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть в базе!");
         }
-        employees.add(toAdd);
-        return toAdd;
+        employees.put(key,employeeToAdd);
+        return employeeToAdd;
     }
 
 
     @Override
-    public Employee find(String firstName, String lastName) throws EmployeeNotFoundException {
-        for (int i = 0; i < employees.size(); i++) {
-            if (employees.get(i).getFirstName().equals(firstName) && employees.get(i).getLastName().equals(lastName)) {
-                return employees.get(i);
-            }
-        }
-        throw new EmployeeNotFoundException("Такой сотрудник не найдем!");
+    public Optional<Employee> find(String key) {
+        return Optional.ofNullable(employees.values().stream()
+                .filter(e -> e.getShortName().equals(key))
+                .findAny()
+                .orElseThrow(() -> new EmployeeNotFoundException("Такой сотрудник не найдем!")));
+    }
+
+    public Optional<Employee> minSalary(int department) {
+        return employees.values().stream()
+                .filter(e -> e.getDepartment() == (department))
+                .min(Comparator.comparingDouble(Employee::getSalary));
+    }
+
+    public Optional<Employee> maxSalary(int department) {
+        return employees.values().stream()
+                .filter(e -> e.getDepartment() == (department))
+                .max(Comparator.comparingDouble(Employee::getSalary));
     }
 
 }
